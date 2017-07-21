133ce76c76423f0b84983a01fb16b0ba829e76a2
neilsatt <neilsatt@gmail.com>
2017-07-20 19:52:52 -0600

    move files over from neilsatt.github.io repo

---
+++ js/site.js
@@ -0,0 +1,25 @@
+function showDate() {
+  var date = new Date();
+  var currentDate = date.toDateString();
+  document.getElementById('time').innerHTML = currentDate;
+}
+
+showDate();
+
+var modal = document.getElementById('myModal');          
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+    
